bff3c7d5eceb3f3b908e8726b26191633242a8fd
sobanAhmed-tech <sobanahmed23200@gmail.com>
2026-08-11 19:28:10 +0500

    Django post website

--- config/settings.py
+++ config/settings.py
@@ -135,6 +135,7 @@ REST_FRAMEWORK = {
     "DEFAULT_FILTER_BACKENDS": [
         "django_filters.rest_framework.DjangoFilterBackend",
         "rest_framework.filters.SearchFilter",
+        
     ],
     "DEFAULT_PAGINATION_CLASS": "blog.pagination.StandardPageNumberPagination",
     "PAGE_SIZE": 10,
